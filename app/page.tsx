55f7fb77cb33ec6f2b49640ed44fc271f611e209
tony2-5 <ajd99@DESKTOP-NL2MGO9>
2024-06-24 10:48:57 -0400

    more vercel fixes(hopefully last one)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
 import { Data } from "@/components/Getdata";
 import { LoginButton, LogoutButton } from "@/components/Auth";
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions"
 import { LoginButton, LogoutButton } from "./Auth";
 import Link from 'next/link'
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/navbar";
